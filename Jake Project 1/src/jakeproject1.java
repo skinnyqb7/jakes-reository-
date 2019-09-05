a7c751b851f3899278a9593b4e8bd69fbf2326a7
jakemccormick <jakemccormick@10.242.0.171>
2019-09-05 12:15:55 -0400

    Project Finsihed

---
+++ Jake Project 1/src/jakeproject1.java
@@ -0,0 +1,22 @@
+// Project Name: Jake Project 1
+public class jakeproject1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I love books");
+		System.out.println("I love school");
+		System.out.println("I love football");
+		System.out.println("I love Food");
+		System.out.println("I Love Chairs ");
+		System.out.println("I love Books");
+		System.out.println("I love Xbox");
+		System.out.println("I love My Friends");
+		System.out.println("I love Music");
+		System.out.println("I love Cookies");
+		System.out.println("I love Chicken Tenders");
+		System.out.println("I love  My Mom");
+	}
+
+
+
+}
