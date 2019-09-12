28ebdfff7e31d33a351fdca07d0e5530012f0144
jakemccormick <jakemccormick@10.242.0.171>
2019-09-11 21:53:20 -0400

    this the hw

---
+++ Jake Project 1/src/projectx/package-info.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+/**
+ * @author jakemccormick
+ *
+ */
+public class jakeproject1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int age=18;
+String name="Jake McCormick";
+int favNum= 7;
+
+System.out.println("Hello My Name IS "+name+" and I am "+age);
+System.out.print("My favorite number is "+favNum);
+	}
+
+
+
+}
