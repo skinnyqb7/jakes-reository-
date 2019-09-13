578e143df3d8f23bb533dd2ecd7c7742fea5bdca
jakemccormick <jakemccormick@10.242.0.171>
2019-09-12 21:27:49 -0400

    Ready to be graded

--- Project 2/src/jakeproject2.java
+++ Project 2/src/jakeproject2.java
@@ -1,18 +1,14 @@
 // Project Name: Jake Project 2
 // Author: Jake Mccormick
 
-public class jakeproject1 {
+public class project2
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int age= 18;
+int age=18;
 String name="Jake McCormick";
 int favNum= 7;
 
 System.out.println("Hello My Name IS "+name+" and I am "+age);
 System.out.print("My favorite number is "+favNum);
-	}
-
-
-
-}
+{
